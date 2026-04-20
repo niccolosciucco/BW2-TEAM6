f28bda2fe2856c698d8f66251a46d641c8ee3acd
Niccolò Sciucco <sciucco.niccolo@gmail.com>
2026-04-20 20:41:37 +0200

    album listened

--- scripts/home-page.js
+++ scripts/home-page.js
@@ -1,3 +1,45 @@
+// #region COMPORTAMENTO CAROSELLO
+
+// const carousel = document.querySelector("#carouselExample");
+// if (window.matchMedia("(min-width: 768px)").matches) {
+//   const carouselInstance = new bootstrap.Carousel(carousel, {
+//     interval: false, // Disattiva lo scorrimento automatico
+//   });
+
+//   let scrollPosition = 0;
+
+//   document
+//     .querySelector(".carousel-control-next")
+//     .addEventListener("click", function () {
+//       const containerWidth =
+//         document.querySelector(".carousel-inner").scrollWidth;
+//       const cardWidth = document.querySelector(".carousel-item").offsetWidth;
+
+//       if (scrollPosition < containerWidth - cardWidth * 4) {
+//         scrollPosition += cardWidth;
+//         document.querySelector(".carousel-inner").scrollTo({
+//           left: scrollPosition,
+//           behavior: "smooth",
+//         });
+//       }
+//     });
+
+//   document
+//     .querySelector(".carousel-control-prev")
+//     .addEventListener("click", function () {
+//       const cardWidth = document.querySelector(".carousel-item").offsetWidth;
+//       if (scrollPosition > 0) {
+//         scrollPosition -= cardWidth;
+//         document.querySelector(".carousel-inner").scrollTo({
+//           left: scrollPosition,
+//           behavior: "smooth",
+//         });
+//       }
+//     });
+// }
+
+// #endregion
+
 class GeneralMusic {
   constructor() {
     this.data = null;
@@ -19,39 +61,42 @@ class GeneralMusic {
 // #region PLAY AND PAUSE MUSIC
 let audio = null;
 
-const handleMusic = (preview, isMobile) => {
-  if (!isMobile) {
-    const btn = document.getElementById("play");
+const handleMusic = (preview, isMobile, isMouseHover = false, idBtn = "") => {
+  if (audio && audio.src !== preview) {
+    audio.pause();
+    audio = new Audio(preview);
+  }
 
-    if (!audio) {
-      audio = new Audio(preview);
-    }
+  if (!audio) {
+    audio = new Audio(preview);
+  }
 
-    if (btn.innerText === "Play") {
-      audio.play();
-      btn.innerText = "Pause";
-      btn.classList.replace("btn-success", "btn-warning");
-    } else {
-      audio.pause();
-      btn.innerText = "Play";
-      btn.classList.replace("btn-warning", "btn-success");
-    }
+  let btn;
+  if (isMobile) {
+    btn = document.getElementById("play-mobile");
+  } else if (isMouseHover) {
+    btn = document.getElementById(idBtn);
   } else {
-    const btn = document.getElementById("play-mobile");
+    btn = document.getElementById("play");
+  }
 
-    if (!audio) {
-      audio = new Audio(preview);
-    }
+  // Logica Play/Pause
+  if (audio.paused) {
+    audio.play();
+    updateBtnIcon(btn, true);
+  } else {
+    audio.pause();
+    updateBtnIcon(btn, false);
+  }
+};
 
-    if (btn.classList.contains("bi-play-circle-fill")) {
-      audio.play();
-      btn.classList.replace("bi-play-circle-fill", "bi-pause-circle-fill");
-      btn.classList.replace("btn-success", "btn-warning");
-    } else {
-      audio.pause();
-      btn.classList.replace("bi-pause-circle-fill", "bi-play-circle-fill");
-      btn.classList.replace("btn-warning", "btn-success");
-    }
+const updateBtnIcon = (btn, isPlaying) => {
+  if (isPlaying) {
+    btn.classList.replace("bi-play-fill", "bi-pause-fill");
+    btn.classList.replace("btn-success", "btn-warning");
+  } else {
+    btn.classList.replace("bi-pause-fill", "bi-play-fill");
+    btn.classList.replace("btn-warning", "btn-success");
   }
 };
 // #endregion
@@ -120,12 +165,66 @@ const idAlbum = [
 // #region ALBUM
 
 const getAlbum = () => {
-  qs = "album/";
-  idAlbum.forEach((id) => {
-    codAlbum = id;
-    fullUrl = urlSearch + qs + codAlbum;
-    console.log(fullUrl);
+  const qs = "album/";
+  const row = document.getElementById("album-music");
+  row.innerHTML = "";
 
+  idAlbum.forEach((id, i) => {
+    const fullUrl = urlSearch + qs + id;
+
+    fetch(fullUrl)
+      .then((response) => {
+        if (response.ok) return response.json();
+        throw new Error("Fetch Errata");
+      })
+      .then((data) => {
+        const idBtn = `btn-album${i}`;
+
+        row.innerHTML += `
+        <div class="col">
+          <div class="card album-card bg-dark bg-gradient text-white overflow-hidden h-100">
+            <div class="row g-0 h-100"> 
+              <div class="col-4">
+                <img src="${data.cover_big}" class="img-fluid rounded-start h-100 object-fit-cover" alt="cover ${data.title}">
+              </div>
+
+              <div class="col-8 position-relative">
+                <div class="card-body d-flex align-items-center h-100">
+                  <h2 class="card-title h6 mb-0 text-truncate">${data.title}</h2>
+                </div>
+              </div>
+
+              <a href="#" 
+                id="${idBtn}"
+                class="play-button position-absolute top-50 end-0 translate-middle-y btn btn-success text-black p-0 d-flex align-items-center justify-content-center rounded-circle bi bi-play-fill me-1" 
+                style="width: 40px; height: 40px; font-size: 1.5rem;"
+                onclick="handleMusic('${data.tracks.data[0].preview}', false, true, '${idBtn}')">
+              </a>
+            </div>
+          </div>
+        </div>
+        `;
+      })
+      .catch((error) => console.error(error));
+  });
+};
+
+getAlbum();
+
+// #endregion
+
+// #region CAROSELLO ALBUM
+
+const idAlbum2 = [552945182, 786324441, 234196722, 926315591, 503137, 6347177];
+const idAlbum3 = [552945182, 786324441, 234196722, 926315591, 503137, 6347177];
+
+const createCarousel = () => {
+  const qs = "album/";
+  const row = document.getElementById("album-listened");
+  row.innerHTML = "";
+
+  idAlbum2.forEach((id, i) => {
+    const fullUrl = urlSearch + qs + id;
     fetch(fullUrl)
       .then((response) => {
         if (response.ok) {
@@ -135,25 +234,25 @@ const getAlbum = () => {
         }
       })
       .then((data) => {
-        const row = document.getElementById("album-music");
+        const row = document.getElementById("album-listened");
+        const idBtn = `btn-album${i + 99999}`;
 
-        row.innerHTML += `
-        <div class="col">
-          <div class="card bg-dark bg-gradient text-white overflow-hidden h-100">
-            <div class="row g-0 h-100"> <div class="col-4">
-                <img src="${data.cover_big}" class="img-fluid rounded-start h-100 object-fit-cover" alt="cover ${data.title}">
-              </div>
+        row.innerHTML += `                            
+<div class="col-6 col-md-3 col-lg-2">
+    <div class="card h-100 album-card bg-transparent border-0 position-relative">
+      <img src="${data.cover_big}" class="card-img-top img-fluid" alt="${data.title}" />
+      <div class="card-body text-secondary p-2">
+        <p class="card-text text-truncate">${data.artist.name}</p>
+      </div>
 
-              <div class="col-8">
-                <div class="card-body d-flex align-items-center h-100">
-                  <h2 class="card-title h6 mb-0 text-truncate">${data.title}</h2>
-                </div>
-              </div>
-
-            </div>
-          </div>
-        </div>
-      `;
+      <a href="javascript:void(0)" 
+        id="${idBtn}"
+        class="play-button position-absolute btn btn-success text-black p-0 d-flex align-items-center justify-content-center rounded-circle bi bi-play-fill" 
+        style="width: 40px; height: 40px; font-size: 1.5rem; bottom: 10px; right: 10px;"
+        onclick="handleMusic('${data.tracks.data[0].preview}', false, true, '${idBtn}')">
+      </a>
+    </div> 
+</div>`;
       })
       .catch((error) => {
         console.log(error);
@@ -161,10 +260,51 @@ const getAlbum = () => {
   });
 };
 
-getAlbum();
+createCarousel();
 
-// #endregion
+const createCarousel2 = () => {
+  const qs = "album/";
+  const row = document.getElementById("album-listened2");
+  row.innerHTML = "";
 
-// #region CAROSELLO ALBUM
+  idAlbum3.forEach((id, i) => {
+    const fullUrl = urlSearch + qs + id;
+    fetch(fullUrl)
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("Fetch Errata", response.status);
+        }
+      })
+      .then((data) => {
+        console.log(data.title);
+        const row = document.getElementById("album-listened2");
+        const idBtn = `btn-album${i + 999999}`;
+
+        row.innerHTML += `                            
+<div class="col-6 col-md-3 col-lg-2">
+    <div class="card h-100 album-card bg-transparent border-0 position-relative">
+      <img src="${data.cover_big}" class="card-img-top img-fluid" alt="${data.title}" />
+      <div class="card-body text-secondary p-2">
+        <p class="card-text text-truncate">${data.artist.name}</p>
+      </div>
+
+      <a href="javascript:void(0)" 
+        id="${idBtn}"
+        class="play-button position-absolute btn btn-success text-black p-0 d-flex align-items-center justify-content-center rounded-circle bi bi-play-fill" 
+        style="width: 40px; height: 40px; font-size: 1.5rem; bottom: 10px; right: 10px;"
+        onclick="handleMusic('${data.tracks.data[0].preview}', false, true, '${idBtn}')">
+      </a>
+    </div> 
+</div>`;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  });
+};
+
+createCarousel2();
 
 // #endregion
