fd6810b7d972c797821c467e3a7fd3c6ffcaadc8
Niccolò Sciucco <sciucco.niccolo@gmail.com>
2026-04-21 16:08:29 +0200

    updated main

--- scripts/home-page.js
+++ scripts/home-page.js
@@ -452,3 +452,11 @@ const syncProgressBar = (audioInstance) => {
   };
 };
 // #endregion
+
+const sidebar = document.getElementById("sidebarLeft");
+const toggleBtn = document.getElementById("toggleSidebar");
+
+toggleBtn.addEventListener("click", () => {
+  sidebar.classList.toggle("sidebar-collapsed");
+  sidebar.classList.toggle("sidebar-expanded");
+});
