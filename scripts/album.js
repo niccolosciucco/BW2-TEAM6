a4fd199dd4a65fcb939cd6b9e67d1769e3aea652
Amir B. Kahna <amir.kahna2@gmail.com>
2026-04-22 13:13:30 +0200

    Fixed buttons and added js features to collapse left sidebar

--- scripts/album.js
+++ scripts/album.js
@@ -238,3 +238,13 @@ const loadArtistInfo = () => {
 
 loadArtistInfo();
 // #endregion
+
+// #region COLLASSO SIDEBAR
+const sidebar = document.getElementById("sidebarLeft");
+const toggleBtn = document.getElementById("toggleSidebar");
+
+toggleBtn.addEventListener("click", () => {
+  sidebar.classList.toggle("sidebar-collapsed");
+  sidebar.classList.toggle("sidebar-expanded");
+});
+// #endregion
